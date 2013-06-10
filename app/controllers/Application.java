e53fbfb7af03864f0fd9e5c6bd23a696ff287976
bgdbraba <bartel.debrabandere@ugent.be>
2013-06-10 12:26:46 +0200

    views made

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(views.html.main.index.render("new application is ready."));
     }
   
 }
