bbc7c9cbfea34e48cc95cdc9503f9bdbdfdf517d
bgdbraba <bartel.debrabandere@ugent.be>
2013-06-18 11:04:59 +0200

    addform child made

---
+++ app/controllers/ChildController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ChildController extends Controller{
+	
+	public static Result registerChild(){
+		return TODO;
+	}
+
+}
