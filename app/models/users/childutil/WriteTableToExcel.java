57bfd3444ced7113e68747af7bbb5fb1cea3a8ee
bartel.debrabandere@ugent.be <bartel.debrabandere@ugent.be>
2014-04-11 00:39:52 +0200

    Class made to write tables to excel.

---
+++ app/models/users/childutil/WriteTableToExcel.java
@@ -0,0 +1,18 @@
+package models.users.childutil;
+
+/**
+ * Class to write children to excel file.
+ * <p/>
+ * Author: Bartel De Brabandere.
+ * Date: 10/04/14
+ * Time: 16:52
+ */
+public class WriteTableToExcel {
+
+    /**
+     * Method to write children to excel.
+     */
+    public static void writeChildrenToExcel(){
+        String fileName = "Children";
+    }
+}
